c8a376cc4bdf7362f03aad57ecafe665892f398d
kordian pawelec <kordianpawelec2@gmail.com>
2026-01-30 23:31:20 +0000

    git push

--- scripts/importand_days.py
+++ scripts/importand_days.py
@@ -1,20 +1,35 @@
 import requests
 import os
+from collections import defaultdict
 
 from datetime import datetime
 
 TOKEN = os.environ.get('DAYS_TOKEN')
-COUNTRY = 'ie'
 YEAR = datetime.now().year
 
 class Holidays:
     def __init__(self):
-        self.url = f'https://calendarific.com/api/v2/holidays?api_key={TOKEN}&country={COUNTRY}&year={YEAR}'
+        pass
 
-    def get_holidays(self):
-        response = requests.get(self.url)
+
+    def get_holidays(self, country: str):
+        response = requests.get(f'https://calendarific.com/api/v2/holidays?api_key={TOKEN}&country={country}&year={YEAR}')
         response.raise_for_status()
-        
+    
         holidays = response.json().get('response').get('holidays')
         
-        return holidays
+        data = []
+        for holiday in holidays:
+            data.append({
+                'name': holiday.get('name'),
+                'date': holiday.get('date').get('iso')})
+
+        return data
+
+    def get_data(self):
+        data = defaultdict(list)
+        for c in ['pl', 'ie']:
+            holidays = self.get_holidays(c)
+            
+            for holiday in holidays:
+                data[holiday['name']].append({'date': holiday['date'], 'country':c})
