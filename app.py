ca3e2494743853236e0c33beb377b4be0f1a1dbe
kordian pawelec <kordianpawelec2@gmail.com>
2026-01-31 10:43:19 +0000

    using fastapi

--- app.py
+++ app.py
@@ -1,36 +1,19 @@
-from flask import Flask
+from fastapi import FastAPI
 from scripts.importand_days import Holidays
-import json
+import uvicorn
 
-app = Flask(__name__)
+app = FastAPI()
 
 holidays = Holidays()
 
-@app.route('/')
+@app.get('/')
 def main():
-    html = "<h1>Holidays</h1><table border='1'><tr><th>Name</th><th>Dates</th></tr>"
-    
-    for names, dates_list in holidays.get_data().items():
-        
-        dates = ''
-        
-        for date in dates_list:
-            dates += f'{date['date']} {date['country']} '
+    return holidays.get_data()
 
-        html += f"<tr><td>{names}</td><td>{dates}</td></tr>"
-    
-    html += "</table>"
-    return html
-
-@app.route('/upcomming')
+@app.get('/upcoming')
 def upcoming():
-    html = "<h1>Upcoming Holidays (Next 7 Days)</h1><table border='1'><tr><th>Name</th><th>Date</th><th>Country</th><th>Days Until</th></tr>"
-    
-    for holiday in holidays.check_close_days():
-        html += f"<tr><td>{holiday['name']}</td><td>{holiday['date']}</td><td>{holiday['country']}</td><td>{holiday['days_until']}</td></tr>"
-    
-    html += "</table>"
-    return html
+    return holidays.check_close_days()
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    uvicorn.run('app:app', reload=True)
