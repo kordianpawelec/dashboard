4a025b4f7f2d3780290b1acd950177968fd75626
kordian pawelec <kordianpawelec2@gmail.com>
2026-02-02 22:00:24 +0000

    caddy

--- run.sh
+++ run.sh
@@ -2,14 +2,5 @@
 set -e
 
 
-docker-compose down
+docker-compose down --remove-orphans
 docker-compose compose up -d
-# git pull
-
-# python3 -m venv .venv
-# . .venv/bin/activate
-
-# pip install -r requirements.txt
-
-# nohup python3 app.py > app.log 2>&1 & echo $!
-# nohup python3 -m scripts.scheduler > scheduler.log 2>&1 & echo $!
