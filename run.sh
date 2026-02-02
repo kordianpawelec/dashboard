bcc32ad41f3f596fc05414c43e3d2af3a8907bb8
kordian pawelec <kordianpawelec2@gmail.com>
2026-02-02 23:34:21 +0000

    commit

--- run.sh
+++ run.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
-git pull
+set -e
 
-# docker-compose down --remove-orphans
 
-docker-compose build dashboard-app
-
-docker-compose up -d --no-deps --scale dashboard-app=2
-
-sleep 5
-
-docker-compose up -d --scale dashboard-app=2
-
-docker image prune -f
+docker-compose down --remove-orphans
+docker-compose up -d
