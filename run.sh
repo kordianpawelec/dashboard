3fbc6d073b61bcb56cae1cbc7de8055a093ee2fb
kordian pawelec <kordianpawelec2@gmail.com>
2026-02-02 18:36:49 +0000

    CICD with actions

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -e
 
-git pull
 
-python3 -m venv .venv
-. .venv/bin/activate
+docker compose up
+# git pull
 
-pip install -r requirements.txt
+# python3 -m venv .venv
+# . .venv/bin/activate
 
-nohup python3 app.py > app.log 2>&1 & echo $!
-nohup python3 -m scripts.scheduler > scheduler.log 2>&1 & echo $!
+# pip install -r requirements.txt
+
+# nohup python3 app.py > app.log 2>&1 & echo $!
+# nohup python3 -m scripts.scheduler > scheduler.log 2>&1 & echo $!
